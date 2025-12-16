84d8768a0acba184d99f1c345d6b5a83224c480e
Jakub Niewczas <niewczas.jakub@gmail.com>
2025-12-16 12:07:49 +0900

    improve relogability

--- src/lib/components/LoginButton/LoginButton.svelte
+++ src/lib/components/LoginButton/LoginButton.svelte
@@ -1,58 +1,69 @@
 <script lang="ts">
-	import {
-		connectWallet,
-		disconnectWallet,
-		walletConnected,
-		connectedWallet
-	} from '$lib/states/walletState'
+	import { connectWallet, disconnectWallet, walletConnected, connectedWallet } from '$lib/states/walletState'
 	import { pbConnected, connectPb, disconnectPb } from '$lib/states/pocketbaseState'
 	import { fetchPocketbaseToken } from './lib'
 
-	let status: 'disconnected' | 'connectingWallet' | 'connectingSystem' | 'connected' | 'errored' = 'disconnected'
+	let status: 'disconnected' | 'walletConnected' | 'systemConnected' | 'errored' = 'disconnected'
 
-	// Keep local status in sync with restored sessions from the stores
 	$: {
-		const waitingOnWallet = status === 'connectingWallet'
-
-		if ($walletConnected && $pbConnected) {
-			if (status !== 'connected') status = 'connected'
+		if ($pbConnected) {
+			status = 'systemConnected'
 		} else if ($walletConnected) {
-			if (status !== 'connectingSystem') status = 'connectingSystem'
-		} else if ($pbConnected) {
-			if (status !== 'connectingWallet') status = 'connectingWallet'
-		} else if (!waitingOnWallet && status !== 'disconnected') {
+			status = 'walletConnected'
+		} else {
 			status = 'disconnected'
 		}
 	}
 
+	$: ((connected: boolean) => {
+		if (!connected) status = 'walletConnected'
+	})($pbConnected)
+
+	$: ((connected: boolean) => {
+		if (!connected) status = 'disconnected'
+	})($walletConnected)
+
 	const login = async () => {
-		status = 'connectingWallet'
+		status = 'disconnected'
 		await connectWallet()
-		status = 'connectingSystem'
+		status = 'walletConnected'
 		const token = await fetchPocketbaseToken($connectedWallet)
 		await connectPb({ token })
-		status = 'connected'
+		status = 'systemConnected'
+	}
+
+	const loginToSystem = async () => {
+		const token = await fetchPocketbaseToken($connectedWallet)
+		await connectPb({ token })
+		status = 'systemConnected'
+	}
+
+	const relog = async () => {
+		await logout()
+		await login()
 	}
 
 	const logout = async () => {
-		await disconnectWallet()
 		await disconnectPb()
+		await disconnectWallet()
 		status = 'disconnected'
 	}
 </script>
 
-<button class="btn primary" on:click={$walletConnected ? logout : login}>
-	<span>
-		{#if status === 'connected'}
-			Logout
-		{:else if status === 'connectingWallet'}
-			Connecting wallet...
-		{:else if status === 'connectingSystem'}
-			Connecting to system...
-		{:else if status === 'errored'}
-			Retry login
-		{:else}
-			Login
-		{/if}
-	</span>
-</button>
+{#if status === 'disconnected'}
+	<button class="btn primary" on:click={login}>
+		<span>Login</span>
+	</button>
+{:else if status === 'walletConnected'}
+	<button class="btn primary" on:click={loginToSystem}>
+		<span>Login to System</span>
+	</button>
+{:else if status === 'systemConnected'}
+	<button class="btn primary" on:click={logout}>
+		<span>Logout</span>
+	</button>
+{:else if status === 'errored'}
+	<button class="btn primary" on:click={relog}>
+		<span>Retry</span>
+	</button>
+{/if}
